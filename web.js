22ed70bab11b544f0d1bb548ad79fb290ce9d787
Marchius <alessio.fagioli@gmail.com>
2013-07-16 22:40:35 +0000

    old web.js

--- web.js
+++ web.js
@@ -1,5 +1,5 @@
 var express = require("express");
-var app = express();
+var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
   var fs = require('fs');
@@ -7,7 +7,7 @@ app.get('/', function(request, response) {
   response.send(buffer.toString());
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
